7f4cedf0107f5b93ea06fac82597ad9d1fb3e165
Ruomu <shaoruomu19@gmail.com>
2024-01-20 21:49:26 +1100

    ChatList added

--- components/home/Navigation/index.tsx
+++ components/home/Navigation/index.tsx
@@ -3,6 +3,7 @@
 import { useAppContext } from "@/components/AppContext";
 import MenuBar from "./MenuBar";
 import ToolBar from "./Toolbar";
+import ChatList from "./ChatList";
 
 const Navigation = () => {
   const {
@@ -13,9 +14,10 @@ const Navigation = () => {
     <nav
       className={`${
         displayNavigation ? "" : "hidden"
-      } relative h-full w-[260px] bg-gray-900 text-gray-300 p-2`}
+      } flex flex-col dark relative h-full w-[260px] bg-gray-900 text-gray-300 p-2`}
     >
       <MenuBar />
+      <ChatList />
       <ToolBar />
     </nav>
   );
