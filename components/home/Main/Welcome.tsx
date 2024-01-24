3f322567542571323f2135a611107e3315c6125a
Ruomu <shaoruomu19@gmail.com>
2024-01-24 12:01:05 +1100

    quote mark fixed

--- components/home/Main/Welcome.tsx
+++ components/home/Main/Welcome.tsx
@@ -5,9 +5,7 @@ export default function Welcome() {
   return (
     <div className="w-full max-w-4xl mx-auto flex flex-col items-center px-4 py-20">
       <ModelSelect />
-      <h1 className="mt-20 text-4xl font-bold">
-        "Free to use ChatGPT models!"
-      </h1>
+      <h1 className="mt-20 text-4xl font-bold">Free to use ChatGPT models!</h1>
       <Example />
     </div>
   );
